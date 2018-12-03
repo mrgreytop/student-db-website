347078ed2a259e9ccdf08ec3283b65879904699f
mrgreytop <tom.price09@hotmail.co.uk>
2018-12-03 14:16:15 +0000

    include, put in

--- sqlQueries.php
+++ sqlQueries.php
@@ -1,7 +1,7 @@
 <?php
 include("error_log.php");
 $err = new errlog("error_log.txt");
-###########
+include 'info.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: ". mysqli_connect_error());
